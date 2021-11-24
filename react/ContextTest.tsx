0129684c2c316655f461b2677337da012a395207
arielabaruffaldi <baruffaldiariela@gmail.com>
2021-11-24 19:21:13 -0300

    step1

--- react/ContextTest.tsx
+++ react/ContextTest.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Counter from './Counter'
-// https://kentcdodds.com/blog/how-to-use-react-context-effectively
 
 const ContextTest: React.FunctionComponent = () => {
   return <Counter></Counter>
